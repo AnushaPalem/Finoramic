fa6c9a1f83c90e66c2ac993ddeb9975d5bc85c9a
AnushaPalem <anusha120817@gmail.com>
2018-01-20 23:25:48 +0530

    Update Anagram_Using_singleArray.c

--- Anagram_Using_singleArray.c
+++ Anagram_Using_singleArray.c
@@ -1,44 +1,43 @@
 #include <stdio.h>
-#include <stdbool.h>
-
-bool anagram(char *s,char *t)
-{
-    int array[256];
-int i;
-
-
-for(i=0;s[i];i++)
-{
-array[s[i]]++;
-}
-
-
-for(i=0;t[i];i++)
-{
-    array[t[i]]--;
-}
-
-
-for(i=0;i<256;i++)
-    if(array[i]!=0)
-    return false;
-return true;
-
-}
-
-int main()
-{
-    
-    char s[] = "cook";
-    char t[] = "cook";
-    if ( anagram(s, t))
-      printf("Anagram");
-    else
-      printf("not an anagram");
+ #include <stdbool.h>
  
-    return 0;
-}
+ bool anagram(char *s,char *t)
+ {
+     int array[256]={0};
+ int i;
+ 
+ 
+ for(i=0;s[i];i++)
+ {
+ array[s[i]]++;
+ }
+ 
+ 
+ for(i=0;t[i];i++)
+ {
+     array[t[i]]--;
+ }
+ 
 
+ for(i=0;i<256;i++)
+     if(array[i]!=0)
+     return false;
+ return true;
+ 
+ }
+ 
+ int main()
+ {
+     
+     char s[] = "cook";
+     char t[] = "coko";
+     if ( anagram(s, t))
+       printf("Anagram");
+     else
+       printf("not an anagram");
+  
+     return 0;
+ }
 
 
 
